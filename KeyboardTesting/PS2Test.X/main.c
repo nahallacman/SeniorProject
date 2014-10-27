43e18597553befeaab16aa1eea7b8235ffc8455d
Cal <nahallacman@gmail.com>
2014-10-27 00:04:14 -0700

    Added a small test project for the keyboard.

---
+++ KeyboardTesting/PS2Test.X/main.c
@@ -0,0 +1,92 @@
+/*
+ * File:   main.c
+ * Author: cal
+ *
+ * Created on October 26, 2014, 8:35 PM
+ */
+
+/******************************************************************************
+ * Software License Agreement
+ *
+ * Copyright � 2011 Microchip Technology Inc.  All rights reserved.
+ * Microchip licenses to you the right to use, modify, copy and distribute
+ * Software only when embedded on a Microchip microcontroller or digital
+ * signal controller, which is integrated into your product or third party
+ * product (pursuant to the sublicense terms in the accompanying license
+ * agreement).
+ *
+ * You should refer to the license agreement accompanying this Software
+ * for additional information regarding your rights and obligations.
+ *
+ * SOFTWARE AND DOCUMENTATION ARE PROVIDED ìAS ISî WITHOUT WARRANTY OF ANY
+ * KIND, EITHER EXPRESS OR IMPLIED, INCLUDING WITHOUT LIMITATION, ANY WARRANTY
+ * OF MERCHANTABILITY, TITLE, NON-INFRINGEMENT AND FITNESS FOR A PARTICULAR
+ * PURPOSE. IN NO EVENT SHALL MICROCHIP OR ITS LICENSORS BE LIABLE OR
+ * OBLIGATED UNDER CONTRACT, NEGLIGENCE, STRICT LIABILITY, CONTRIBUTION,
+ * BREACH OF WARRANTY, OR OTHER LEGAL EQUITABLE THEORY ANY DIRECT OR INDIRECT
+ * DAMAGES OR EXPENSES INCLUDING BUT NOT LIMITED TO ANY INCIDENTAL, SPECIAL,
+ * INDIRECT, PUNITIVE OR CONSEQUENTIAL DAMAGES, LOST PROFITS OR LOST DATA,
+ * COST OF PROCUREMENT OF SUBSTITUTE GOODS, TECHNOLOGY, SERVICES, OR ANY
+ * CLAIMS BY THIRD PARTIES (INCLUDING BUT NOT LIMITED TO ANY DEFENSE THEREOF),
+ * OR OTHER SIMILAR COSTS.
+ *
+ *****************************************************************************/
+
+
+/*
+ The purpose of this project is to use the SOLDERLESS breadboard with the pic32mx795L512 H,  NOT L
+ to test the keyboard interaction
+ */
+
+#include <p32xxxx.h>
+#include <plib.h>
+
+//	Function Prototypes
+int main(void);
+
+
+#pragma config	ICESEL	=	ICS_PGx1	// ICE pin selection
+
+
+
+#pragma config POSCMOD=XT, FNOSC=PRIPLL, FPLLIDIV=DIV_2, FPLLMUL=MUL_18, FPLLODIV=DIV_1
+#pragma config FPBDIV=DIV_1, FWDTEN=OFF, CP=OFF, BWP=OFF
+#pragma config FSOSCEN=OFF, IESO=OFF
+
+
+int main(void) {
+
+    //system config
+    //SYSTEMConfig(72000000L, SYS_CFG_WAIT_STATES | SYS_CFG_PCACHE);
+
+    //using RD6 for PS2CLK   //  PORTDbits.RD6;
+    TRISDbits.TRISD6 = 1; // set to input
+    //using RD7 for PS2DATA
+    TRISDbits.TRISD7 = 1; // set to input
+
+    //using internal pullup resistors
+    CNPUEbits.CNPUE15 = 1; //PS2CLK pullup
+    CNPUEbits.CNPUE16 = 1; //PS2DATA pullup
+
+
+
+    int i = 0;
+    int delay = 0;
+    int MAX_DELAY = 1000;
+    while (1)
+    {
+        if(PORTDbits.RD6 == 0)
+        {
+            i = 0;
+            for( delay = 0; delay < MAX_DELAY; delay++);
+        }
+        else
+        {
+            i = 1;
+            for( delay = 0; delay < MAX_DELAY; delay++);
+        }
+    }
+}
+
+
+
