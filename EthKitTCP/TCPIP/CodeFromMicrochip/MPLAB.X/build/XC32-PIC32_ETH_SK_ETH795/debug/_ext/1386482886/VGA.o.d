8da295752f5400ba37ef2c1c1ea3de236c8a64e8
Cal Barkman <callahan.barkman@gmail.com>
2015-03-07 04:42:24 -0800

    Few alterations, working on hardware

--- EthKitTCP/TCPIP/CodeFromMicrochip/MPLAB.X/build/XC32-PIC32_ETH_SK_ETH795/debug/_ext/1386482886/VGA.o.d
+++ EthKitTCP/TCPIP/CodeFromMicrochip/MPLAB.X/build/XC32-PIC32_ETH_SK_ETH795/debug/_ext/1386482886/VGA.o.d
@@ -1,5 +1,4 @@
 build/XC32-PIC32_ETH_SK_ETH795/debug/_ext/1386482886/VGA.o:  \
  ../../../Dev/VGA.c  \
 ../../../Dev/VGA.h  \
-../../../Dev/PS2Common.h  \
- ../../../Dev/TestCommon.h 
+../../../Dev/TestCommon.h 
